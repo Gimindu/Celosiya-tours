f158ee5f6a3726ba806620a90c830e9c38d67538
Niketh-Randil <127325580+Niketh-Randil@users.noreply.github.com>
2024-12-25 13:17:51 +0530

    Mobile responsive done

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -13,23 +13,20 @@ export default function AboutUs() {
 
         {/* Left corner text */}
         <div className="absolute left-4 sm:left-8 top-[35%] sm:top-[45%] text-white max-w-xs sm:max-w-lg">
-          <h2 className="text-2xl sm:text-4xl font-bold leading-tight text-center sm:text-left">
+          <h2 className="text-2xl sm:text-4xl font-bold leading-tight text-left sm:text-left">
             We Provide You The Best <br /> Srilankan Sightseeing Tours
           </h2>
-          <p className="mt-2 sm:mt-4 text-sm sm:text-lg text-center sm:text-left">
+          <p className="mt-2 sm:mt-4 text-sm sm:text-lg text-left sm:text-left">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
 
           <button className="bg-[#DF6951] text-white px-4 py-2 rounded-full hover:bg-[#D56C4C] transition duration-300 mt-4">
             View Packages
           </button>
-
-          
         </div>
-        
 
         {/* Right center image */}
-        <div className="absolute top-[50%] sm:top-[65%] right-2 sm:right-4 transform -translate-y-1/2">
+        <div className="absolute top-[calc(100%+1rem)] sm:top-[45%] right-2 sm:right-4 transform -translate-y-[115%] sm:translate-y-0">
           <img src={rightImage} alt="Right Image" className="w-30 sm:w-30 h-auto" />
         </div>
       </div>
